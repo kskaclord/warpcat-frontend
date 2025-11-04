697cdbff13f52386b06d0f35a6333855f63b164a
kskaclord <31724441+kskaclord@users.noreply.github.com>
2025-11-04 11:04:37 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { optimizeCss: true }
+  // ÖNEMLİ: static export KAPALI (bu dosyada output: 'export' YOK)
+  images: { unoptimized: true }, // og.png için cache/opt. gereksiz
 };
+
 module.exports = nextConfig;
